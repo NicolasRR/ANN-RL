9780e17241d6fcfbd73f0c0ae6414b43a4c8803b
NicolasRR <nreateguir@gmail.com>
2024-05-19 16:30:12 +0200

    up

--- dqnv2.py
+++ dqnv2.py
@@ -23,17 +23,18 @@ class DQNAgentV2(DQNAgent):
         epsilon_decay: float,
         reward_hidden_size :int,
         discount_factor: float = 0.95,
-        dropout_rate: float = 0.1,
+        dropout_rate: float = 0.0,
         weight_decay: float = 0.01,
+        predictor_weight_decay: float = 0.01,
         scheduler=False,
         target_network = False,
-        target_network_update = 100,
-        alpha = 1,
+        target_network_update = 10_000,
+        alpha = 0.0,
         amsgrad = False,
         device = "cpu",
         reward_factor = 1/5,
         running_window = 100,
-
+        predictor_learning_rate = 1e-4,
     ):
         super().__init__(state_size=state_size, action_size=action_size, hidden_size=hidden_size, replay_size=replay_size, learning_rate=learning_rate, initial_epsilon=initial_epsilon, final_epsilon=final_epsilon, epsilon_decay=epsilon_decay, discount_factor=discount_factor, dropout_rate=dropout_rate, weight_decay=weight_decay, scheduler=scheduler, target_network=target_network, target_network_update=target_network_update, alpha=alpha, amsgrad=amsgrad, device=device)        
         """Initialize a Reinforcement Learning agent with an empty dictionary
@@ -53,7 +54,7 @@ class DQNAgentV2(DQNAgent):
         self.criterion_intrinsic = nn.MSELoss()
         self.mse_buffer = None
         self.reward_factor = reward_factor
-        self.optimizer_intrinsic = torch.optim.Adam(self.predictor.parameters(), lr=learning_rate, weight_decay=weight_decay, amsgrad=amsgrad)
+        self.optimizer_intrinsic = torch.optim.Adam(self.predictor.parameters(), lr=predictor_learning_rate, weight_decay=predictor_weight_decay, amsgrad=amsgrad)
         self.target_predictor.eval()
         self.predictor.train()
         self.running_window = running_window
@@ -65,44 +66,42 @@ class DQNAgentV2(DQNAgent):
         reward: float,
         next_obs: tuple[int, int, bool],
         target_count: int,
+        terminal: bool = False,
         batch_size: int = 32,
     ):
 
-        self.replay_buffer.add(obs, action, reward, next_obs)
-        
-        #################
-        self.predictor.train()
-        self.optimizer_intrinsic.zero_grad()
-        predicted_intrinsic = self.predictor(torch.tensor(next_obs, device=self.device).unsqueeze(0))
-        target_intrinsic = self.target_predictor(torch.tensor(next_obs, device=self.device).unsqueeze(0))
-        intrinsic_loss = self.criterion_intrinsic(predicted_intrinsic, target_intrinsic)
-
-        #################
-
-        sample_replay = self.replay_buffer.sample(batch_size, self.alpha)
-        if sample_replay is None:
+        if self.mse_buffer is None or self.mse_buffer.shape[0] < self.running_window:
+            with torch.no_grad():
+                predicted_intrinsic = self.predictor(torch.tensor(next_obs, device=self.device).unsqueeze(0))
+                target_intrinsic = self.target_predictor(torch.tensor(next_obs, device=self.device).unsqueeze(0))
+                intrinsic_loss = self.criterion_intrinsic(predicted_intrinsic, target_intrinsic)
             if self.mse_buffer is None:
                 self.mse_buffer = np.array([intrinsic_loss.item()])
             else:
-                self.mse_buffer = np.hstack((self.mse_buffer[-self.running_window:], intrinsic_loss.item()))
-            return None, target_count, None, None
-        
-        intrinsic_loss.backward()
-        self.optimizer_intrinsic.step()
-        #################
-        with torch.no_grad():
-            self.predictor.eval()
+                self.mse_buffer = np.hstack((self.mse_buffer,intrinsic_loss.item()))
+
+        else:
+            self.optimizer_intrinsic.zero_grad()
             means = np.mean(self.replay_buffer.state_buffer[-self.running_window:], axis=0)
             SD = np.std(self.replay_buffer.state_buffer[-self.running_window:], axis=0)
             normalized_obs = torch.tensor((next_obs-means)/SD, device=self.device).unsqueeze(0)
             predicted_intrinsic = self.predictor(normalized_obs)
             target_intrinsic = self.target_predictor(normalized_obs)
-            loss = self.criterion_intrinsic(predicted_intrinsic, target_intrinsic)
-            RND = self.reward_factor*np.clip((loss.item()-np.mean(self.mse_buffer[-self.running_window:]))/np.std(self.mse_buffer[-self.running_window:]), -5,5)
-            self.mse_buffer = np.hstack((self.mse_buffer[-self.running_window:], RND))
-        
-        reward+=RND
-        #################
+            intrinsic_loss = self.criterion_intrinsic(predicted_intrinsic, target_intrinsic)
+            RND = self.reward_factor*np.clip((intrinsic_loss.item()-np.mean(self.mse_buffer[-self.running_window:]))/np.std(self.mse_buffer[-self.running_window:]), -5,5)
+            self.mse_buffer = np.hstack((self.mse_buffer[-self.running_window:], intrinsic_loss.item()))
+            if self.mse_buffer.shape[0] >= self.running_window:
+                intrinsic_loss.backward()
+                self.optimizer_intrinsic.step()
+            reward+=RND
+
+        if terminal:
+            next_obs = (None, None)
+
+        self.replay_buffer.add(obs, action, reward, next_obs)
+        sample_replay = self.replay_buffer.sample(batch_size, self.alpha)
+        if sample_replay is None:
+            return None, target_count, None, None
         idx = sample_replay[-1]
         replay_reward = torch.tensor(sample_replay[2]).to(self.device)
         actions = torch.tensor(sample_replay[1]).to(self.device)
@@ -168,20 +167,25 @@ def run(args, env):
     batch_size = args.batch_size
     discount_factor = args.discount_factor
     replay_size = args.replay_size
-    logging_interval = args.logging_interval
     hidden_size=args.hidden_size
     dropout_rate=  args.dropout_rate
     weight_decay=args.weight_decay
     target_network = args.target_network,
     target_network_update = int(args.target_network_update)
     alpha = args.alpha
+    amsgrad = args.amsgrad
+    reward_hidden_size=args.reward_hidden_size
+    predictor_learning_rate = args.predictor_learning_rate
+    reward_factor = args.reward_factor
+    running_window = args.running_window
+    predictor_weight_decay = args.predictor_weight_decay
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     random.seed(args.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
 
-    agent = DQNAgent(
+    agent = DQNAgentV2(
         learning_rate=learning_rate,
         state_size=2,
         action_size=3,
@@ -196,65 +200,62 @@ def run(args, env):
         weight_decay=weight_decay,
         target_network_update=target_network_update,
         alpha=alpha,
-        seed=args.seed,
-        amsgrad=args.amsgrad,
-        device = 'cuda' if (torch.cuda.is_available() and args.gpu) else 'cpu'
-
+        amsgrad=amsgrad,
+        reward_hidden_size=reward_hidden_size,
+        reward_factor=reward_factor,
+        running_window=running_window,
+        predictor_learning_rate=predictor_learning_rate,
+        predictor_weight_decay=predictor_weight_decay
     )
     if args.wandb:
-        wandb.init(project='ANN', config={"learning_rate": learning_rate, "n_episodes": n_episodes, "start_epsilon": start_epsilon, "final_epsilon": final_epsilon, "epsilon_decay": epsilon_decay, "batch_size": batch_size, "discount_factor": discount_factor, "replay_size": replay_size, "hidden_size": hidden_size, "dropout_rate": dropout_rate, "weight_decay":weight_decay, "target_network":target_network, "alpha":alpha,"target_network_update":target_network_update,"reward_factor":args.reward_factor, "amsgrad":args.amsgrad, "reward_hidden_size":args.reward_hidden_size}, name='DQN')
+        wandb.init(project='ANN', config={"learning_rate": learning_rate, "n_episodes": n_episodes, "start_epsilon": start_epsilon, "final_epsilon": final_epsilon, "epsilon_decay": epsilon_decay, "batch_size": batch_size, "discount_factor": discount_factor, "replay_size": replay_size, "hidden_size": hidden_size, "dropout_rate": dropout_rate, "weight_decay":weight_decay, "target_network":target_network, "alpha":alpha,"target_network_update":target_network_update, "reward_factor":reward_factor, "reward_hidden_size":reward_hidden_size, "amsgrad":amsgrad}, name='DQNv2')
 
 
     env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
     with tqdm(total=n_episodes, desc=f"Episode 0/{n_episodes}") as pbar:
-        losses = []
-        rewards = []
         target_count = 0
-        finished = []
-        episode_steps = []
+        finished = 0
         empty = True
-        
+        cumulative_auxiliary_reward = 0
+        cumulative_env_reward = 0
         for episode in tqdm(range(n_episodes)):
             obs, info = env.reset()
             done = False
             # play one episode
             t = 0
-            episode_reward = 0
+            episode_auxiliary_reward = 0
+            episode_env_reward = 0
             episode_loss = 0
             
             while not done:
                 action = agent.get_action(obs, env)
-                next_obs, reward, terminated, truncated, info = env.step(action)
+                next_obs, env_reward, terminated, truncated, info = env.step(action)
 
                 # update if the environment is done and the current obs
                 done = terminated or truncated
-                if terminated:
-                    next_obs = (None, None)
-                loss, target_count, RND, predictor_intrinsic_loss = agent.update(obs, action, reward, next_obs, batch_size=batch_size, target_count=target_count)
+
+                loss, target_count, RND,intrinsic_loss = agent.update(obs, action, env_reward, next_obs, batch_size=batch_size, target_count=target_count, terminal=terminated)
                     
                 if loss is not None:
-                    episode_reward += reward+RND
+                    episode_auxiliary_reward += RND
+                    episode_env_reward += env_reward
                     episode_loss+=loss
                 obs = next_obs
                 t+=1
 
             pbar.set_description(f"Episode {episode + 1}/{n_episodes}")
-            pbar.set_postfix(train_loss=loss, epsilon=agent.epsilon, target_count=target_count)
+            pbar.set_postfix(train_loss=episode_loss, epsilon=agent.epsilon, target_count=target_count, episode_steps=t, episode_auxiliary_reward=episode_auxiliary_reward, episode_env_reward=episode_env_reward, intrinsic_loss=intrinsic_loss, finished=finished)
             pbar.update(1)
             pbar.refresh() 
             if not empty:
-                finished.append(terminated)
-                episode_steps.append(t)
-                rewards.append(episode_reward)
-                losses.append(episode_loss)
+                finished += terminated
+                cumulative_auxiliary_reward += episode_auxiliary_reward
+                cumulative_env_reward += episode_env_reward
+
                 agent.decay_epsilon()
-                if episode % logging_interval == 0 :
-                    if args.wandb:
-                        wandb.log({"train_loss": np.mean(losses), "epsilon": agent.epsilon, "episode_steps": np.mean(episode_steps), "finished": np.sum(finished), "mean_reward": np.mean(rewards)})
-                    losses = []
-                    rewards = []
-                    finished = []
-                    episode_steps = []
+                if args.wandb:
+                    wandb.log({"train_loss": episode_loss, "epsilon": agent.epsilon, "episode_steps": t, "finished": finished, "episode_env_reward":episode_env_reward, "episode_aux_reward":episode_auxiliary_reward, "cumulative_env_reward":cumulative_env_reward, "cumulative_aux_reward":cumulative_auxiliary_reward})
+
                     
             if loss is not None:
                 empty = False
@@ -274,15 +275,17 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--discount_factor", type=float, default=0.95)
     parser.add_argument("--replay_size", type=int, default=10_000)
-    parser.add_argument("--logging_interval", type=int, default=10)
     parser.add_argument("--hidden_size", type=int, default=128)
-    parser.add_argument("--dropout_rate", type=float, default=0.0)
-    parser.add_argument("--reward_hidden_size", type=int, default=64)
+    parser.add_argument("--dropout_rate", type=float, default=0.1)
     parser.add_argument("--weight_decay", type=float, default=0.01)
-    parser.add_argument("--reward_factor", type=float, default=1/5)
     parser.add_argument("--target_network_update", type=int, default=10_000)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--alpha", type=float, default=0.0)
+    parser.add_argument("--reward_hidden_size", type=int, default=64)
+    parser.add_argument("--predictor_learning_rate", type=float, default=1e-4)
+    parser.add_argument("--reward_factor", type=float, default=1/5)
+    parser.add_argument("--running_window", type=int, default=100)
+    parser.add_argument("--predictor_weight_decay", type=float, default=1e-4)
     parser.add_argument("--wandb", action="store_true")
     parser.add_argument("--amsgrad", action="store_true")
     parser.add_argument("--target_network", action="store_true")
